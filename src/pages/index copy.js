316b08d28632de03bb1b7e3b265f6ed4d30e34bf
Amir Maleki <realmalekiamir@gmail.com>
2023-07-11 08:08:51 +0330

    remove unnecessary index.js copy file

--- src/pages/index copy.js
+++
@@ -1,8 +0,0 @@
-export { default as HomePage } from "./Home/index";
-export { default as RootHome } from "./RootHome/index";
-export {default as PhonePage} from "./PhonePage/index";
-export {default as Test} from "./Test/index";
-export {default as SearchPage} from "./SearchPage/index";
-export {default as IntroduceProductPage} from "./IntroduceProductPage/index";
-export {default as LoginPage} from "./LoginPage/index";
-export {default as ErrorPage} from "./Error/index";
